6560ac3f068458f8c5d86ffbcaa9a4278ee483b6
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2025-06-13 19:48:36 -0300

    Adiciona inicialização do banco e rota Flask para cadastro e listagem de médicos

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from app.config import Config
+from app.models.database import init_db, SessionLocal
+from app.models.doctor import Doctor
+from app.models.specialization import Specialization
 import os
 
 app = Flask(__name__, template_folder="app/views", static_folder="static")
 
 
+@app.before_first_request
+def initialize_database():
+    init_db()
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route('/doctors', methods=['GET', 'POST'])
+def doctors():
+    session = SessionLocal()
+    if request.method == 'POST':
+        name = request.form['name']
+        specialization_id = request.form['specialization_id']
+        doctor = Doctor(name=name, specialization_id=specialization_id)
+        session.add(doctor)
+        session.commit()
+        return redirect(url_for('doctors'))
+    doctors = session.query(Doctor).all()
+    specializations = session.query(Specialization).all()
+    return render_template('doctor_form.html', doctors=doctors, specializations=specializations)
 
 
 def create_app():
